f6f6ae03d1e0f0e8fbb5b5c6cfc4317bc4613445
MarkDemidovs <markdemidovs@proton.me>
2024-10-08 18:49:56 +0300

    Basic Backend through Express.js

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import {PORT} from "./config.js";
+
+const app = express();
+
+app.get('/', (req,res) => {
+    console.log(req);
+    return res.status(234).send("Welcome to ManabuCode!")
+    
+})
+
+app.listen(PORT, ()=>{
+    console.log(`listening on port ${PORT}`);
+})
